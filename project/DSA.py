e52a4e606d70641ead912497aaa2da390d167464
Max2Min <haivo5229@gmail.com>
2025-10-12 06:09:36 +0900

    reverse linked list 2

--- project/DSA.py
+++ project/DSA.py
@@ -1,4 +1,4 @@
-from algorithms.LinkedListCycle import *
+from algorithms.ReverseLinkedList import *
 from dataStructures import *
 
-print(Solution().hasCycle(cycleLinkedList([1], -1)))
+print(Solution().reverseBetween(linkedList([5]), 1, 1))
--- project/algorithms/ReverseLinkedList.py
+++ project/algorithms/ReverseLinkedList.py
@@ -6,7 +6,25 @@ class Solution:
         if head == None or head.next == None:
             return head
         
-        reversed = Solution().reverseList(head.next)
+        reversed = self.reverseList(head.next)
+
         head.next.next = head
         head.next = None
+
         return reversed
+    
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if right == 1:
+            return head
+        
+        left-=1
+        right-=1
+        reversed = self.reverseBetween(head.next, left, right)
+        
+        tail = reversed
+        if left < 1:
+            tail = head.next.next
+            head.next.next = head
+        head.next = tail
+
+        return reversed if left < 1 else head
