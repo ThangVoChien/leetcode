81b16995e5060579eb880c5d772b168c8cc4eff4
Max2Min <haivo5229@gmail.com>
2025-11-03 18:09:31 +0900

    remove duplicates from sorted array ii

--- project/DSA.py
+++ project/DSA.py
@@ -1,4 +1,4 @@
 from algorithms.RemoveDuplicatesFromSortedArray import *
 from dataStructures import *
 
-print(Solution().removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+print(Solution().removeDuplicates2([0,0,1,1,1,1,2,3,3]))
--- project/algorithms/RemoveDuplicatesFromSortedArray.py
+++ project/algorithms/RemoveDuplicatesFromSortedArray.py
@@ -20,3 +20,32 @@ class Solution:
             ptr+=1
 
         return dup
+    
+    def removeDuplicates2(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        dict ={}
+
+        dup = 0
+        cur = 0
+        ptr = 0
+
+        while ptr < len(nums):
+            num = nums[ptr]
+
+            if num in dict:
+                if dict[num] >= 2:
+                    ptr+=1
+                    continue
+            else:
+                dict[num] = 0
+
+            nums[cur] = nums[ptr]
+            dict[num] += 1
+
+            cur+=1
+            dup+=1
+            ptr+=1
+
+        return dup
