489343dc1b7165da5ad503511731aa9cc1e79379
Max2Min <haivo5229@gmail.com>
2025-11-12 23:06:47 +0900

    search in rotated sorted array

--- project/algorithms/SearchInRotatedSortedArray.py
+++ project/algorithms/SearchInRotatedSortedArray.py
@@ -17,9 +17,42 @@ class Solution:
             if nums[mid] == target:
                 return mid
             elif (rotated == r and target < nums[mid]) or (rotated != r and target > nums[mid]):
-                print(rotated, r)
                 end = mid - 1
             else:
                 start = mid + 1
 
         return -1
+
+    def search2(self, nums: List[int], target: int) -> bool:
+        k = nums[0]
+        if target == k:
+            return True
+        if target < k and target > nums[len(nums)-1]:
+            return False
+
+        rotated = target < k
+
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = (end + start) // 2
+
+            i = mid
+            if mid != 0 and nums[mid] == k:
+                i-=1
+                while i > 0 and nums[i] == k:
+                    i-=1
+
+                if nums[i] == target:
+                    return True
+
+            r = nums[i] < k
+
+            if nums[mid] == target:
+                return True
+            elif (rotated == r and target < nums[mid]) or (rotated != r and target > nums[mid]):
+                end = mid - 1
+            else:
+                start = mid + 1
+
+        return False
