c0cedd79e95a9027b200fe563708c01c81457cff
Max2Min <haivo5229@gmail.com>
2026-01-03 23:28:55 +0700

    unique paths 2

--- project/algorithms/UniquePaths.py
+++ project/algorithms/UniquePaths.py
@@ -14,20 +14,15 @@ class Solution:
         return dynamic(0, 0)
     
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        visited = [[0] * len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
+        visited = [[-1] * len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
         def dynamic(i, j):
-            if obstacleGrid[i][j] == 1:
+            if i == len(obstacleGrid) or j == len(obstacleGrid[0]) or obstacleGrid[i][j] == 1:
                 return 0
             if i == len(obstacleGrid)-1 and j == len(obstacleGrid[0])-1:
                 return 1
-            if visited[i][j] != 0:
+            if visited[i][j] != -1:
                 return visited[i][j]
             
-            if i == len(obstacleGrid)-1:
-                visited[i][j] = dynamic(i, j+1)
-            elif j == len(obstacleGrid[0])-1:
-                visited[i][j] = dynamic(i+1, j)
-            else:
-                visited[i][j] = dynamic(i+1, j) + dynamic(i, j+1)
+            visited[i][j] = dynamic(i+1, j) + dynamic(i, j+1)
             return visited[i][j]
         return dynamic(0, 0)
