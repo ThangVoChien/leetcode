0900a988160d6288ed649a3db16311b511c9a13f
Max2Min <haivo5229@gmail.com>
2026-01-21 06:32:32 +0700

    set matrix zeroes

--- project/DSA.py
+++ project/DSA.py
@@ -1,4 +1,4 @@
-from algorithms.EditDistance import *
+from algorithms.SetMatrixZeroes import *
 from dataStructures import *
 
-print(Solution().minDistance("intention", "execution"))
+print(Solution().setZeroes([[1,2,3,4],[5,0,7,8],[0,10,11,12],[13,14,15,0]]))
