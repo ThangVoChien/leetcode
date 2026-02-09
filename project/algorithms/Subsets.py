822e4d95b61eabe5e715e7992d4fced41711d4ff
Max2Min <haivo5229@gmail.com>
2026-02-09 21:59:35 +0700

    subsets 2

--- project/algorithms/Subsets.py
+++ project/algorithms/Subsets.py
@@ -18,3 +18,18 @@ class Solution:
         generate("0"*len(nums))
 
         return sols
+    
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        def backtrack(start, path):
+            res.append(path[:])
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                backtrack(i + 1, path + [nums[i]])
+
+        backtrack(0, [])
+        return res
