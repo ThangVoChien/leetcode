ff5cd412c22246d40c9d1ee04ba7b6d8af9c36bf
Max2Min <haivo5229@gmail.com>
2026-03-14 23:55:32 +0700

    binary tree level order traversal 2

---
+++ project/algorithms/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,9 @@
+from typing import List, Optional
+from dataStructures.BinaryTree import TreeNode
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        pass
+
+    def levelOrderBottom2(self, root: Optional[TreeNode]) -> List[List[int]]:
+        pass
---
+++ project/algorithms/BinaryTreeZigzagLevelOrderTraversal.py
@@ -0,0 +1,6 @@
+from typing import List, Optional
+from dataStructures.BinaryTree import TreeNode
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        pass
