296513e5ed257277c136bc6462c31e60f2cf7671
Max2Min <haivo5229@gmail.com>
2026-05-18 22:17:34 +0700

    next greater element 4

--- project/algorithms/NextGreaterElement.py
+++ project/algorithms/NextGreaterElement.py
@@ -74,3 +74,22 @@ class Solution:
         result = int(''.join(digits))
 
         return result if result <= 2**31 - 1 else -1
+    
+    def secondGreaterElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        output = [-1] * n
+        stack = []
+        second = []
+        for i in range(n):
+            while second and nums[i] > nums[second[-1]]:
+                curr = second.pop()
+                if output[curr] == -1:
+                    output[curr] = nums[i]
+            temp = []
+            while stack and nums[i] > nums[stack[-1]]:
+                curr = stack.pop()
+                if output[curr] == -1:
+                    temp.append(curr)
+            stack.append(i)
+            second += temp[::-1]
+        return output
