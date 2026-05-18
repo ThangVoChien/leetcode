4e76e13954e8d43d1853858fa5e705ba40cc1787
Max2Min <haivo5229@gmail.com>
2026-05-18 22:14:26 +0700

    next greater element 3

--- project/algorithms/NextGreaterElement.py
+++ project/algorithms/NextGreaterElement.py
@@ -51,3 +51,26 @@ class Solution:
                 i = len(nums)-1
             else:
                 i-=1
+
+    def nextGreaterElement3(self, n):
+        digits = list(str(n))
+        length = len(digits)
+        pivot = -1
+
+        for i in range(length - 2, -1, -1):
+            if digits[i] < digits[i + 1]:
+                pivot = i
+                break
+
+        if pivot == -1:
+            return -1
+
+        for i in range(length - 1, pivot, -1):
+            if digits[i] > digits[pivot]:
+                digits[i], digits[pivot] = digits[pivot], digits[i]
+                break
+
+        digits[pivot + 1:] = digits[pivot + 1:][::-1]
+        result = int(''.join(digits))
+
+        return result if result <= 2**31 - 1 else -1
