cb6b906537b9510463d36882c0b8e6c93ab0096a
Max2Min <haivo5229@gmail.com>
2026-05-22 23:59:01 +0700

    find if path exists in graph

--- project/DSA.py
+++ project/DSA.py
@@ -1,4 +1,4 @@
-from algorithms.FindTheTownJudge import *
+from algorithms.FindIfPathExistsInGraph import *
 from dataStructures import *
 
-print(Solution().findJudge(3, [[1,3],[2,3],[3,1]]))
+print(Solution().validPath(6, [[0,1],[0,2],[3,5],[5,4],[4,3]], 0, 5))
---
+++ project/algorithms/FindIfPathExistsInGraph.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        parent = list(range(n))
+        rank = [1] * n
+
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(node1, node2):
+            root1 = find(node1)
+            root2 = find(node2)
+            if root1 != root2:
+                if rank[root1] > rank[root2]:
+                    parent[root2] = root1
+                elif rank[root2] > rank[root1]:
+                    parent[root1] = root2
+                else:
+                    parent[root2] = root1
+                    rank[root1]+=1
+
+        for e in edges:
+            union(e[0], e[1])
+
+        return find(source) == find(destination)
