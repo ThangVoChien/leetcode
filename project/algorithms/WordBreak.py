70f30135ccbf85261f14c577f34c1a2f4d02d328
Max2Min <haivo5229@gmail.com>
2026-06-01 22:44:45 +0700

    word break 2

--- project/algorithms/WordBreak.py
+++ project/algorithms/WordBreak.py
@@ -12,3 +12,17 @@ class Solution:
                     break
         
         return dp[-1]
+    
+    def wordBreak2(self, s: str, wordDict: List[str]) -> List[str]:
+        ans = []
+
+        def dfs(pref: str, suff: str)-> None:
+            if suff == '':
+                ans.append(pref.rstrip())    
+            else:
+                for i in range(len(suff)):
+                    if suff[:i+1] not in wordDict: continue
+                    dfs(pref + suff[:i+1]+ ' ', suff[i+1:])
+        dfs('', s)
+
+        return ans
