1c7470c85a6858ce600b7a6faefadf853411c559
Max2Min <haivo5229@gmail.com>
2026-06-25 13:19:54 +0700

    maximum gap

---
+++ project/algorithms/MaximumGap.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        nums.sort()
+
+        sol = 0
+        for i in range(1, len(nums)):
+            s = abs(nums[i]-nums[i-1])
+            sol = max(sol,s)
+        return sol
