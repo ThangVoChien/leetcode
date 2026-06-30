32ab8c82f74e32a9d0d7b8d056e73cc513d93fa0
Max2Min <haivo5229@gmail.com>
2026-07-01 04:57:33 +0700

    two sum 4 input is a bst

--- project/algorithms/TwoSum.py
+++ project/algorithms/TwoSum.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Optional
+from dataStructures.BinaryTree import TreeNode
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -17,13 +18,26 @@ class Solution:
         end = len(numbers) - 1
 
         while start < end:
-            total = numbers[start] + numbers[end]
+            sum = numbers[start] + numbers[end]
 
-            if total == target:
+            if sum == target:
                 return [start + 1, end + 1]
-            elif total < target:
+            elif sum < target:
                 start += 1
             else:
                 end -= 1
 
         return [-1, -1]
+    
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        map = set()
+
+        def dfs(node):
+            if not node:
+                return False
+            if k - node.val in map:
+                return True
+            map.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+
+        return dfs(root)
