c75abba7388c8456fdf3b2cf89067009ae760d81
Max2Min <haivo5229@gmail.com>
2026-07-15 11:59:08 +0700

    reshape data melt

--- project/algorithms/ReshapeData.py
+++ project/algorithms/ReshapeData.py
@@ -4,7 +4,7 @@ def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df1, df2])
     
 def meltTable(report: pd.DataFrame) -> pd.DataFrame:
-    pass
+    return report.melt(id_vars=['product'], var_name='quarter', value_name='sales')
     
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
     pass
